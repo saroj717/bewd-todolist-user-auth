3361feddc1ea8eceedce15975123c18713331124
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2024-03-15 10:42:42 -0600

    new commit

---
+++ db/migrate/20240315032843_add_user_id_to_sessions.rb
@@ -0,0 +1,6 @@
+class AddUserIdToSessions < ActiveRecord::Migration[6.1]
+  def change
+    add_belongs_to :sessions, :user
+
+  end
+end
---
+++ db/migrate/20240315033425_add_token_to_sessions.rb
@@ -0,0 +1,6 @@
+class AddTokenToSessions < ActiveRecord::Migration[6.1]
+  def change
+    add_column :sessions, :token, :string
+
+  end
+end
